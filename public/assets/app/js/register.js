d81f4f8461a2d307a47df3568aab04caa734dcb9
horly <andmatboy@gmail.com>
2023-07-12 13:08:16 +0100

    save contact online user

--- public/assets/app/js/register.js
+++ public/assets/app/js/register.js
@@ -205,3 +205,27 @@ $("#hide-password-confirm").click(function(){
     $('#show-password-confirm').removeClass('d-none');
     $('#password-confirm').attr('type', 'password');
 });
+
+/**
+ * save contact
+ */
+var saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", function () {
+  // Get the contact information from the website
+  var contact = {
+    name: $('#name-contact').text(),
+    phone: $('#phone-contact').text(),
+    email: $('#email-contact').text()
+  };
+  // create a vcard file
+  var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+  var blob = new Blob([vcard], { type: "text/vcard" });
+  var url = URL.createObjectURL(blob);
+  
+  const newLink = document.createElement('a');
+  newLink.download = contact.name + ".vcf";
+  newLink.textContent = contact.name;
+  newLink.href = url;
+  
+  newLink.click();
+});
--- resources/views/main/infos-online-user.blade.php
+++ resources/views/main/infos-online-user.blade.php
@@ -16,7 +16,7 @@
                         <img src="{{ asset('assets/img/profile/card') }}/{{ $user->matricule }}.png" class="rounded-circle mx-auto d-block mb-4" alt="..." width="250">
 
                         <div class="mb-5">
-                            <h1 class="text-center text-blue-theme">{{ $user->name }}</h1>
+                            <h1 class="text-center text-blue-theme" id="name-contact">{{ $user->name }}</h1>
                             <p class="text-center text-muted">{{ $grade->name }}</p>
                         </div>
 
@@ -28,12 +28,20 @@
 
                         <div class="row align-items-start">
                             <h6 class="col">Téléphone</h6>
-                            <h6 class="col text-blue-theme text-end">{{ $user->phone_number }}</h6>
+                            <h6 class="col text-blue-theme text-end" id="phone-contact">{{ $user->phone_number }}</h6>
                         </div>
 
                         <div class="row align-items-start">
                             <h6 class="col">Email</h6>
-                            <h6 class="col text-blue-theme text-end">{{ $user->email }}</h6>
+                            <h6 class="col text-blue-theme text-end" id="email-contact">{{ $user->email }}</h6>
+                        </div>
+
+                        <div class="row align-items-start">
+                            <h6 class="col">Site web</h6>
+                            <a href="https://www.exadgroup.org" target="_blank" class="col text-blue-theme text-end h6">www.exadgroup.org</a>
+                        </div>
+                        <div class="d-grid gap-2 mt-4">
+                            <button class="btn btn-primary" type="button" id="save-btn"><i class="fa-solid fa-address-card"></i> Enregistrer le contact</button>
                         </div>
                     </div> 
                     <div class="card-footer bg-blue-theme" style="height: 30px"></div>   
